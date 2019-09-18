43de8f9e3640728d4e5017d04f94fc2f185fa560
Mykola Shchipskyi <m.schipskyi@gmail.com>
2019-09-18 21:35:59 +0300

    Added explain work of branches

--- ITEAHub.CoffeeShop.BusinessLogic/Run.cs
+++ ITEAHub.CoffeeShop.BusinessLogic/Run.cs
@@ -12,6 +12,21 @@ namespace ITEAHub.CoffeeShop.BusinessLogic
         private readonly ICashRegister cashRegister;
         public Run()
         {
+            // How to work with branches
+            // Clone a project from github
+            // your current branch will be master
+            // you have to create a new branch
+            // right click -> new local branch from -> master
+            // implementing 
+            // commit 
+            // check out to master branch
+            // make a pull
+            // check out back to your branch
+            // merge in your branch from master
+            // and then check out to master 
+            // and make again merge to master from your branch
+            // push to server on github
+
            //cashRegister = new //
            // menu = null2;
            // Create another objects
